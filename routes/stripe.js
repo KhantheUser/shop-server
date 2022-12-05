407dce5fe28d16fbe7fbc74c21121691f8455724
Thien Duc <ducthiennguyen1601@gmail.com>
2022-12-05 21:53:08 +0700

    '21'

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,20 +6,7 @@ const stripe = require('stripe')('sk_test_51MB7GvBVx1VB4V21wHa9Sxm8CxSYvuQObv5Fo
 
 router.route('/payment')
 .post(async(req,res,next)=>{
-  // const products = req.body.map((product,index)=>{
-    //   return {
-      //     price_data: {
-        //         currency: 'usd',
-        //         unit_amount: req.body.price,
-        //         product_data: {
-  //           name: req.body.name,
-  //           description: 'my love forever',
-  //           images: ['https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg'],
-  //         },
-  //       },
-  //       quantity: 1,
-  // }
-  // })
+ 
   const URL = process.env.URL_CLIENT
   const products = req.body.map((product,index)=>{
     return {
