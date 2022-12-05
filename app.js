01834bc5eecd1e7713ed41284267f82bcfe7686e
Thien Duc <ducthiennguyen1601@gmail.com>
2022-12-05 17:51:33 +0700

    '3'

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
 const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
-const productRoutes = require('./routes/productRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const stripeRoutes = require('./routes/stripe');
-const cors = require('cors')
-const AppError = require('./util/appError')
-const globalErrorHandler = require('./controllers/errorController');
-dotenv.config(
-    {
-        path :'./.env'
-    }
-);
+// const morgan = require('morgan');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// const userRoutes = require('./routes/userRoutes');
+// const authRoutes = require('./routes/authRoutes');
+// const productRoutes = require('./routes/productRoutes');
+// const cartRoutes = require('./routes/cartRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
+// const stripeRoutes = require('./routes/stripe');
+// const cors = require('cors')
+// const AppError = require('./util/appError')
+// const globalErrorHandler = require('./controllers/errorController');
+// dotenv.config(
+//     {
+//         path :'./.env'
+//     }
+// );
 
 const app = express();
-app.use(express.static('public'));
-app.use(express.json());
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-  }
+// app.use(express.static('public'));
+// app.use(express.json());
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'));
+//   }
 
-app.use(cors());
-app.use('/api/auth',authRoutes)
-app.use('/api/user',userRoutes)
-app.use('/api/product',productRoutes)
-app.use('/api/cart',cartRoutes)
-app.use('/api/order',orderRoutes)
-app.use('/api/checkout',stripeRoutes)
-
-
+// app.use(cors());
+// app.use('/api/auth',authRoutes)
+// app.use('/api/user',userRoutes)
+// app.use('/api/product',productRoutes)
+// app.use('/api/cart',cartRoutes)
+// app.use('/api/order',orderRoutes)
+// app.use('/api/checkout',stripeRoutes)
 
 
-app.all('*', (req, res, next) => {
+
+
+// app.all('*', (req, res, next) => {
     
-    next(new AppError(`Can not find ${req.originalUrl}`,404))
-  })
-  app.use(globalErrorHandler)
+//     next(new AppError(`Can not find ${req.originalUrl}`,404))
+//   })
+//   app.use(globalErrorHandler)
 
 
-const DATABASE = process.env.DATABASE_URL.replace('<password>',process.env.DATABASE_PASSWORD);
+// const DATABASE = process.env.DATABASE_URL.replace('<password>',process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DATABASE).then(()=>{
-    console.log('Connected to database');
-}).catch(err => console(err));
+// mongoose.connect(DATABASE).then(()=>{
+//     console.log('Connected to database');
+// }).catch(err => console(err));
+
+app.get('/api/product',(req,res)=>{
+  res.send('hellooooo')
+})
+
 
 module.exports = app;
